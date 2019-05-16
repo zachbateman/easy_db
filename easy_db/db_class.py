a6a16adc0f0e9a40686e16432e8d9640dce04f56
Zach Bateman <zkbateman@gmail.com>
2019-05-16 11:56:32 -0500

    Add database type logic and connection methods; Add database SELECT * query methods

--- easy_db/db_class.py
+++ easy_db/db_class.py
@@ -3,7 +3,13 @@ Module containing easy_db DataBase class.
 '''
 import sqlite3
 import pyodbc
+import os
+import time
 from functools import lru_cache
+from functools import wraps
+import .util
+
+
 
 
 class DataBase():
@@ -11,52 +17,111 @@ class DataBase():
 
     def __init__(self, db_location_str: str='') -> None:
         self.db_location_str = db_location_str
+
         self.db_type = self.find_db_type()
+        if self.db_type == 'ACCESS':
+            self.connection = self.connection_access
+        elif self.db_type == 'SQL SERVER':
+            self.connection = self.connection_sql_server
+        elif self.db_type == 'SQLITE3':
+            self.connection = self.connection_sqlite
 
 
 
     def find_db_type(self):
-
-        return ''
-
-
-    def connection_sqlite(self):
         '''
+        Figure out what kind of databse is being used.
         '''
-        # FIRST check to see if db file exists?!
-        return sqlite3.connect(self.db_location_str)
+
+        if '.accdb' in self.db_location_str or '.mdb' in self.db_location_str:
+            return 'ACCESS'
+        elif 'DSN' in self.db_location_str:
+            return 'SQL SERVER'
+        elif util.check_if_file_is_sqlite(self.db_location_str):
+            return 'SQLITE3'
+        else:
+            return 'Database not recognized!'
 
 
-    def connection_access(self):
+    def connection_sqlite(self, also_cursor: bool=False):
         '''
+        Return a connection object to the Sqlite3 Database.
+        '''
+        conn = sqlite3.connect(self.db_location_str)
+        if also_cursor:
+            return conn, conn.cursor()
+        else:
+            return conn
+
+
+    def connection_access(self, also_cursor: bool=False):
+        '''
+        Return a connection object to the Access Database.
         '''
         conn = pyodbc.connect(
         r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
         r'Dbq=' + self.db_location_str + ';')
-        return conn
+        if also_cursor:
+            return conn, conn.cursor()
+        else:
+            return conn
 
 
-    def connection_sql_server(self):
+    def connection_sql_server(self, also_cursor: bool=False):
         '''
+        Return a connection object to the SQL Server Database.
         '''
-        return pyodbc.connect(self.db_location_str)
+        conn = pyodbc.connect(self.db_location_str)
+        if also_cursor:
+            return conn, conn.cursor()
+        else:
+            return conn
+
+
+    def provide_db_connection(func, also_cursor=False):
+        '''
+        Decorator provides db connection (and cursor if requested) for database.
+        To be used to decorate functions that would like to manipulate conn/cursor directly.
+        '''
+        @wraps(func)  # allows for decorated func's docstring to come through decorator
+        def inner(*args, **kwargs):
+            if also_cursor:
+                conn, cursor = self.connect(also_cursor=True)
+                cursor.arraysize = 50  # attempt to speed up cursor.fetchall() calls... not sure of impact
+                returned = func(conn, cursor, *args, **kwargs)
+            else:
+                conn = self.connect()
+                returned = func(conn, *args, **kwargs)
+            try:
+                conn.commit()
+            except:
+                time.sleep(0.1)  # if database is locked
+                conn.commit()
+            finally:
+                conn.close()
+            return returned
+        return inner
 
 
     @lru_cache(maxsize=4)
     def pull_full_table(self, tablename: str) -> list:
         '''
-
+        SELECT * Query for full table as specified from tablename.
+        Return list of dicts for rows with column names as keys.
         '''
         sql = f'SELECT * FROM {tablename};'
-        return []
+        with self.connection(also_cursor=True) as conn, cursor:
+            return util.list_of_dicts_from_query(cursor, sql, tablename)
 
 
-    def pull_table_where(self, tablename: str, condition) -> list:
+    def pull_table_where(self, tablename: str, condition: str) -> list:
         '''
-
+        SELECT * WHERE Query for table as specified from tablename and condition
+        Return list of dicts for rows with column names as keys.
         '''
         sql = f'SELECT * FROM {tablename} WHERE {condition};'
-        return []
+        with self.connection(also_cursor=True) as conn, cursor:
+            return util.list_of_dicts_from_query(cursor, sql, tablename)
 
 
 
