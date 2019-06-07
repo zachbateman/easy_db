8a0c34534848fd6206df2b256ffd1c0a5bf2ce04
Zach Bateman <zkbateman@gmail.com>
2019-06-07 00:23:06 -0500

    Switch setup.py to use setuptools; update .gitignore

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from distutils.core import setup
+import setuptools
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-setup(name='easy_db',
-      version='0.1.0',
-      packages=['easy_db'],
-      license='MIT',
-      author='Zach Bateman',
-      description='Easy Python database interaction',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/zachbateman/easy_db.git',
-      download_url='https://github.com/zachbateman/easy_db/archive/v_0.1.0.tar.gz',
-      keywords=['DATABASE', 'SIMPLE', 'EASY'],
-      install_requires=['easy_multip', 'pyodbc', 'tqdm'],
-      classifiers=['Development Status :: 3 - Alpha',
+setuptools.setup(
+    name='easy_db',
+    version='0.1.0',
+    packages=['easy_db'],
+    license='MIT',
+    author='Zach Bateman',
+    description='Easy Python database interaction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zachbateman/easy_db.git',
+    download_url='https://github.com/zachbateman/easy_db/archive/v_0.1.0.tar.gz',
+    keywords=['DATABASE', 'SIMPLE', 'EASY'],
+    install_requires=['easy_multip', 'pyodbc', 'tqdm'],
+    classifiers=['Development Status :: 3 - Alpha',
                    'License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6',
