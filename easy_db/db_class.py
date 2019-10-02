a3bedf7a75d68d02083c29018f7663acbd201147
Zach Bateman <zkbateman@gmail.com>
2019-10-02 11:48:05 -0500

    Add datetime type handling in sqlite table creation; increase version to 0.4.3 for release

--- easy_db/db_class.py
+++ easy_db/db_class.py
@@ -286,6 +286,7 @@ class DataBase():
                         'str': 'TEXT',
                         'text': 'TEXT',
                         'varchar': 'TEXT',
+                        'datetime': 'DATE',
                         }
             column_types = ', '.join([f'{k} {type_map[v]}' for k, v in columns_and_types.items()])
             sql = f"CREATE TABLE '{tablename}'({column_types});"
